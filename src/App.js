9e02f1e2f5518ede50355d06b2348b38beed62e7
m.bezrodnyi <maks.bezrodnyi@gmail.com>
2020-10-21 23:38:06 +0300

    input, useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import TodoList from "./TODO/todoList";
+import AddTodo from "./TODO/AddTodo";
 import context from "./context";
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    { id: 1, compleate: false, title: "text1" },
-    { id: 2, compleate: true, title: "text2" },
-    { id: 3, compleate: false, title: "text3" },
+  const [todos, setTodos] = useState([
+    { id: 2561, compleate: false, title: "text1" },
+    { id: 892, compleate: true, title: "text2" },
+    { id: 783, compleate: false, title: "text3" },
   ]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/?_limit=10")
+      .then((response) => response.json())
+      .then((json) => setTodos([...todos, ...json]));
+  }, []);
 
   function onChange(data) {
     setTodos(
@@ -25,11 +32,27 @@ function App() {
     setTodos(todos.filter((item) => item.id !== id));
   }
 
+  function addTodo(value) {
+    let maxId = Math.max(...todos.map((item) => item.id));
+
+    let newTodo = {
+      id: ++maxId,
+      compleate: false,
+      title: value,
+    };
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <context.Provider value={{ removeTodo }}>
       <div className="wrapper">
-        <h1>TUTORIAL</h1>
-        <TodoList todos={todos} onToggle={onChange}></TodoList>
+        <h1 className="center">TUTORIAL</h1>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={onChange} />
+        ) : (
+          <p className="center">EMPTY LIST</p>
+        )}
       </div>
     </context.Provider>
   );
